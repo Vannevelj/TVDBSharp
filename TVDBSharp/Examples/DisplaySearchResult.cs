1f96f302da9d18160cb78682d4d8e3d9e784b82d
Jeroen Vannevel <jer_vannevel@hotmail.com>
2014-04-02 14:19:33 +0200

    Cleaned gitignore and sample file

--- TVDBSharp/Examples/DisplaySearchResult.cs
+++ TVDBSharp/Examples/DisplaySearchResult.cs
@@ -6,11 +6,10 @@ namespace Examples {
     public class DisplaySearchResult {
         public DisplaySearchResult() {
             var apikey = ConfigurationManager.AppSettings["apikey"]; // Your own API key
-            Console.WriteLine(apikey);
-            var series = "o";
+            var series = "Battlestar Galactica";
 
             var tvdb = new TVDB(apikey);
-            var result = tvdb.Search(series, 100);
+            var result = tvdb.Search(series, 3);
 
             foreach (var show in result) {
                 Console.WriteLine("{0}:\t{1}", show.Name, show.ID);
