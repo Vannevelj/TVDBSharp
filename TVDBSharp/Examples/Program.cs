d64fa80202425f1da598bd539abcc65224437fb6
Vannevelj <jer_vannevel@outlook.com>
2020-03-12 21:09:31 +0000

    removed using statements

--- TVDBSharp/Examples/Program.cs
+++ TVDBSharp/Examples/Program.cs
@@ -1,11 +1,7 @@
 using System;
 using System.Configuration;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Threading;
 using TVDBSharp;
-using TVDBSharp.Models.Enums;
 
 namespace Examples
 {
